d33dd0934881098bdd808bb1045a2cbd3c1559ff
Mil Hoornaert <mil@Mil>
2023-12-17 14:31:03 +0100

    first commit

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ A disscussion forum built using ReactJS, NodeJS and MongoDB.
 
 - Redirect to `/server` and run `$ npm run dev` in terminal. The server will start on port 5000 and gets connected to mongoDB.
 - Now, in another terminal, redirect to `client` and run `$ npm start`. It will start the client side of the application.
+# forum
--- client/src/components/common/jumbotron.jsx
+++ client/src/components/common/jumbotron.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Jumotron = () => {
   return (
     <div className="jumbotron">
-      <h1 className="display-3">Community</h1>
-      <p class="lead">We &lt;3 people who code!</p>
+      <h1 className="display-3">Improve. Forum</h1>
+      <p class="lead">We &lt;3 to learn and improve!</p>
       <hr class="my-4" />
-      <p className="lead">A disscussion forum for enthusiastic developers.</p>
+      <p className="lead">A disscussion forum for people who want to share wisdom to help others.</p>
     </div>
   );
 };
